8a44a730f321d6341ad5758369c0c64d04a8da67
Ishan Kumar Anand <ishananand2002@gmail.com>
2024-05-20 21:10:21 +0530

    Modification in Array and Vector

--- Lecture9/1_array.cpp
+++ Lecture9/1_array.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+/*
 int main()
 {
     // int a[5];
@@ -33,8 +34,64 @@ int main()
         }
         // or min(elem1, elem2) inbuilt function
     }
-    cout << "Max is " << max << " Min is " << min << " and Sum is " << sumarr;
+    cout << "Max is " << max << " Min is " << min << " and Sum is " << sumarr << endl;
+
+    //---------------------------//
+
+    int arr[3];
+    fill(arr, arr + 3, 17); // it fills all the elements with same number (start index, end index, value )
+    cout << arr[0] << " " << arr[1] << " " << arr[2] << " ";
 }
+
+*/
 
 // IMP - lets say if an araay of size 5 and only 2 values are initialized
 //  in it what will be rest 3 spaces of array contain, it's 0's and not Grbage Value
+
+/*
+Taking Input in Array
+*/
+
+int main()
+{
+    int arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+/*
+
+inc size cin>> size;
+int arr[size];
+
+The above code is a bad practise to initialize the array, Memory Allocation Issue
+
+Explanation:
+
+
+*/
+
+/*
+Why any array index begins with 0, why not 1
+
+So, the formula to find element present at index i is [Base Address + size of array data type * i]
+
+ex - Array with vaues   arr[ 10 | 60 | 20 | 63 | 74 | 55 ]
+address of above array  400, 404, 408, 412, 416
+
+if i = 1 the  apply the above formula [400 + 4 * 1 ] this gives element 60 and not element 10
+hence index always starts with 0
+
+*/
+
+/*
+Whenever we pass an array  in function it's address value is always passed so modification
+will take place in original array
+*/
