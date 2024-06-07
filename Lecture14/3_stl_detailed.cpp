264f3083d5c518539a38908dc829187ce92860be
Ishushan02 <ishananand2002@gmail.com>
2024-06-07 22:23:06 +0530

    STL Vector

---
+++ Lecture14/3_stl_detailed.cpp
@@ -0,0 +1,33 @@
+# include <iostream>
+
+using namespace std;
+
+
+/*
+Containers
+
+
+
+
+*/
+
+// Vector
+
+# include <vector>
+int main(){
+
+    vector <int> marks;
+    vector <int> miles(10);
+    vector <int> distances (15, 0);
+
+    /*
+    All Methods
+    vector.begin(), vector.front(), vector.back(), vector.end(), vector.size(), vector.empty(),vector.capacity()
+    vector.reverse(size_type n), 
+    vector.push_back(value), vector.pop_back(), vector.insert(iterator position, value), vector.erase(iterator position)
+    vector.clear(), vector.at(), swap()
+    
+    */
+
+
+}
