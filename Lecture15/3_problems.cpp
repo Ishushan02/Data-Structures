dcdf2160756c5e9f7f253490f6b764ef9bcd6f9f
Ishan Kumar Anand <ishananand2002@gmail.com>
2024-07-16 21:20:20 +0530

    Integer to Roman

--- Lecture15/3_problems.cpp
+++ Lecture15/3_problems.cpp
@@ -374,4 +374,54 @@ public:
 };
 
 
+// This type of String Compression is also known as Run Length Encoding
+
+*/
+
+/*
+
+12. Integer to Roman (https://leetcode.com/problems/integer-to-roman/description/)
+
+string intToRoman(int num) {
+        map<int, string> values = {
+            {1, "I"},
+            {4, "IV"},
+            {5, "V"},
+            {9, "IX"},
+            {10, "X"},
+            {40, "XL"},
+            {50, "L"},
+            {90, "XC"},
+            {100, "C"},
+            {400, "CD"},
+            {500, "D"},
+            {900, "CM"},
+            {1000, "M"}
+        };
+        string ans;
+        // iterating reverse in Map
+        map<int, string>::reverse_iterator it = values.rbegin();
+        while(it != values.rend()){
+
+            if (num < 0){
+                break;
+            }
+            int data = it->first;
+            string s = it->second;
+
+            if(data <= num){
+                cout << data << endl;
+                ans += s;
+                num = num - data;
+                it = values.rbegin();
+                continue;
+            }
+            it++;
+        }
+
+        return ans;
+
+
+    }
+
 */
