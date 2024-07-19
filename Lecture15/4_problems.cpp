a440fda891bcb7d8acd548e930d1ca791a0c2fb1
Ishan Kumar Anand <ishananand2002@gmail.com>
2024-07-19 18:09:46 +0530

    Greates Number VVI Question

--- Lecture15/4_problems.cpp
+++ Lecture15/4_problems.cpp
@@ -78,3 +78,38 @@ using namespace std;
         return ans;
     }
 */
+
+/*
+
+179. Largest Number   https://leetcode.com/problems/largest-number/description/
+VVI Question
+
+static bool comprator(string a, string b){
+        string t1 = a + b;
+        string t2 = b + a;
+
+        return t1>t2; // think of 303 & 330so we must return 330 when {3, 30} are given.
+    }
+
+    string largestNumber(vector<int>& nums) {
+        vector<string>  sortedstr;
+
+        for(int i = 0; i < nums.size();i++){
+            sortedstr.push_back(to_string(nums[i]));
+        }
+        sort(sortedstr.begin(), sortedstr.end(), comprator);
+
+        string ans;
+        for(int i = 0; i < nums.size();i++){
+            ans += sortedstr[i];
+        }
+
+        // edge case of 0
+        if (sortedstr[0] == "0") {
+            return "0";
+        }
+
+        return ans;
+    }
+
+*/
