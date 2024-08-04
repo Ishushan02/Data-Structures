7c04a49cf1000a6c02a4a840d6b21b908e7d0f51
Ishan Kumar Anand <ishananand2002@gmail.com>
2024-08-04 19:38:47 +0530

    Reverse a String using Recursion

--- Lecture17/4_problems.cpp
+++ Lecture17/4_problems.cpp
@@ -12,7 +12,7 @@ We Can also  do this using strrchr() function of string
 int lastoccurence(string &word, int index, int ans, char c)
 {
 
-    if (index == word.length())
+    if (index == (int)word.length())
     {
         return ans;
     }
@@ -25,12 +25,26 @@ int lastoccurence(string &word, int index, int ans, char c)
     lastoccurence(word, index + 1, ans, c);
 }
 
+void reverseString(string &word, int startIndex, int endIndex)
+{
+    if (startIndex > endIndex)
+    {
+        return;
+    }
+
+    swap(word[startIndex], word[endIndex]);
+
+    reverseString(word, startIndex + 1, endIndex - 1);
+}
+
 int main()
 {
 
-    string word = "abcddedg";
-    char c = 'd';
-    int ans = lastoccurence(word, 0, -1, c);
+    string word = "abcddedgp";
+    // char c = 'd';
+    // int ans = lastoccurence(word, 0, -1, c);
+    // cout << " Last Occurence of the char " << c << " is " << ans << endl;
 
-    cout << " Last Occurence of the char " << c << " is " << ans << endl;
+    reverseString(word, 0, word.length() - 1);
+    cout << " Reverse of a String abcddedgp" << " is " << word << endl;
 }
