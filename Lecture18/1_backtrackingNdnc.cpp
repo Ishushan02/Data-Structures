5a8bb527a8ffe6950e9c485bc5b0b413d66d2f96
Ishushan02 <ishananand2002@gmail.com>
2024-08-15 20:29:24 +0530

    Merge Sort

--- Lecture18/1_backtrackingNdnc.cpp
+++ Lecture18/1_backtrackingNdnc.cpp
@@ -39,22 +39,84 @@ void mergeSort(int arr1[], int n1, int arr2[], int n2, vector<int> & res ){
 }
 
 
-// Merge
+// Merge Sort
+
+// merge the given array
+void merge(vector<int>& arr, int start, int mid, int end){
+
+    vector<int> ans;
+    // we have 2 array's we need to sort that
+    int leftarrStart = start;
+    int leftarrEnd = mid;
+    int rightarrStart = mid+1;
+    int rightarrEnd = end;
+
+    while(leftarrStart <= leftarrEnd && rightarrStart <= rightarrEnd){
+        if(arr[leftarrStart] < arr[rightarrStart]){
+            ans.push_back(arr[leftarrStart]);
+            leftarrStart++;
+        }else{
+            ans.push_back(arr[rightarrStart]);
+            rightarrStart++;
+        }
+    }
+
+    while(leftarrStart <= leftarrEnd){
+        ans.push_back(arr[leftarrStart]);
+        leftarrStart++;
+    }
+
+    while(rightarrStart <= rightarrEnd){
+        ans.push_back(arr[rightarrStart]);
+        rightarrStart++;
+    }
+
+    for(int i = start; i <= end; i++){
+        arr[i] = ans[i - start];
+    }
+
+}
+
+// divide the given array in 2 parts
+void divide(vector<int> & arr, int start, int end){
+    if(start >= end){
+        return;
+    }
+
+    int mid = (start + end) / 2;
+    divide(arr, start, mid);
+    divide(arr, mid+1, end);
+    
+    merge(arr, start, mid, end);
+
+}
 
 
 int main(){
-    int arr1[] = {1, 11, 17, 38, 45, 70};
-    int arr2[] = {2, 8, 15, 18, 24, 30, 34, 48};
-    int n1 = 6;
-    int n2 = 8;
+    // int arr1[] = {1, 11, 17, 38, 45, 70};
+    // int arr2[] = {2, 8, 15, 18, 24, 30, 34, 48};
+    // int n1 = 6;
+    // int n2 = 8;
 
-    vector<int> res;
-    mergeSort(arr1, n1, arr2, n2, res);
+    // vector<int> res;
+    // mergeSort(arr1, n1, arr2, n2, res);
 
-    cout << " Merge Sort Sorted Arrays " << endl;
-    for(auto i : res){
+    // cout << " Merge Sort Sorted Arrays " << endl;
+    // for(auto i : res){
+    //     cout << i << " ";
+    // }
+
+    // -------------------------------------------------------------------------------------------
+
+    vector<int> arr = {98, 43, 19, 10, 90, 63, 12, 1};
+    // vector<int> ans;
+    divide(arr, 0, 7);
+
+    cout << " Merged Sorted Arrays " << endl;
+    for(auto i : arr){
         cout << i << " ";
     }
+
 
 }
 
