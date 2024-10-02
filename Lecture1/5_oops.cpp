88ab28806c622c05b5ba6023d07349ddf746e537
ishushan02 <ishananand2002@gmail.com>
2024-10-02 07:06:20 -0700

    Abstraction

---
+++ Lecture1/5_oops.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+/*
+
+Abstraction
+    - Delivering only essential details to ouside world while masking the ackground details.
+    - It is a design and programming method that seperates interface from implementation
+    - Let's take sort function sort(vector.being(), vctor.end()) or it also take sort(string.begin(), string.end())
+        so inspite of anything given we doesn't care about internal functioning and thinks of output (sorted elements)
+
+    - Abstract does is creates interface and on that interface and once you write the code in interface, it 
+
+*/
+
