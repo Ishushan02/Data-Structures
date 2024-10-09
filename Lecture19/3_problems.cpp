17a673803f6d0d8aa90b7082ef26f20362442f41
ishushan02 <ishananand2002@gmail.com>
2024-10-08 21:40:14 -0700

    Middle Node

--- Lecture19/3_problems.cpp
+++ Lecture19/3_problems.cpp
@@ -26,3 +26,37 @@ int main(){
         return prev;
     }
 */
+
+
+/*
+
+876. Middle of the Linked List
+(https://leetcode.com/problems/middle-of-the-linked-list/submissions/1416594390/)
+
+    ListNode* middleNode(ListNode* head) {
+        vector<int> value;
+
+        ListNode* tempNode = head;
+        int count = 0;
+        while(head){
+            value.push_back(head->val);
+            count++;
+            head = head->next;
+        }
+
+        
+            //odd
+            int temp = 0;
+            while(tempNode){
+                if(temp == count/2){
+                    return tempNode;
+                }
+                temp++;
+                tempNode = tempNode->next;
+            }
+        
+        return head;
+
+        
+    }
+*/
