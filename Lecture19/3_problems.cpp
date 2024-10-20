c41365e725b745fdb374bfd9c67b58fbf688907a
ishushan02 <ishananand2002@gmail.com>
2024-10-20 00:17:34 -0700

    Add 2 LinkedList Numbers using Recursion

--- Lecture19/3_problems.cpp
+++ Lecture19/3_problems.cpp
@@ -514,6 +514,37 @@ int main(){
         return ans;
     }
 
+
+
+
+    // --------------------------------------------------------------------
+    // Recursive Approach
+
+
+    ListNode* recursiveAdd(ListNode* l1, ListNode* l2, int carry){
+
+        if(!l1 && !l2 && !carry){return 0; }
+
+        int a = l1 ? l1->val:0;
+        int b = l2 ? l2->val:0;
+        int sum = a + b + carry;
+        int digit = sum % 10;
+        carry = sum / 10;
+
+        ListNode* ans = new ListNode(digit);
+        l1 = l1 ? l1->next : l1;
+        l2 = l2 ? l2->next : l2;
+        ans->next = recursiveAdd(l1, l2, carry);
+
+        return ans;
+
+
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        return recursiveAdd(l1, l2, 0);
+    }
+
 */
 
 /*
