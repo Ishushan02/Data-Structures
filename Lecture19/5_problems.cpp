e792e739c4d6def8bfc81914e1a45bf27ef9710c
ishushan02 <ishananand2002@gmail.com>
2024-10-26 06:42:42 -0700

    Deep Copy Important 2nd Method

--- Lecture19/5_problems.cpp
+++ Lecture19/5_problems.cpp
@@ -39,6 +39,65 @@ int main(){
         unordered_map<Node*, Node*> mp;
         return recursemap(head, mp);
     }
+
+    //------------------------------------------------------------------------------------------------
+
+    // Method 2 Takes O(n) times as map isn't created in it and no extra O(n) space is required
+    void print(Node* head){
+        Node* temp = head;
+
+        while(temp){
+            cout << temp->val << " ";
+            temp = temp->next;
+        }
+    }
+
+    Node* copyRandomList(Node* head) {
+        
+        if (head == NULL){
+            return NULL;
+        }
+
+        // Step 1, Clone A (7->13->11->10->1 : 7->7->13->13->11->11->10->10->1->1)
+
+        Node* it = head;
+        while(it){
+            Node* clonedNode = new Node(it->val);
+            clonedNode->next = it->next;
+            it->next = clonedNode;
+            it = it->next->next;
+
+        }
+
+        it = head;
+        // print(it);
+        // Step 2, create the connections between new Node and there random values
+
+        while(it){
+            Node* clonedNode = it->next;
+            clonedNode->random = it->random ? it->random->next:nullptr;
+            it = it->next->next;
+        }
+
+        // Step 3, remove the links between old and new Nodes
+        it = head;
+        Node* clonedHead = it->next;
+        while(it){
+            Node* clonedNode = it->next;
+            it->next = clonedNode->next;
+            if(clonedNode->next){
+                clonedNode->next = clonedNode->next->next;
+            }
+            it = it->next;
+
+        }
+
+        return clonedHead;
+
+
+    }
+
+
 */
 
 
