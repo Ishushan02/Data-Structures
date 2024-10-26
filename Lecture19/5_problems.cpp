2ee116ad000a8685fa0f7a6428c9da979b2f7355
ishushan02 <ishananand2002@gmail.com>
2024-10-26 07:18:02 -0700

    Rotate List

--- Lecture19/5_problems.cpp
+++ Lecture19/5_problems.cpp
@@ -101,3 +101,68 @@ int main(){
 */
 
 
+/*
+
+    61. Rotate List
+    (https://leetcode.com/problems/rotate-list/description/)
+
+
+    long length(ListNode* head){
+        long len = 0;
+        ListNode* temp = head;
+
+        while(temp){
+            len += 1;
+            temp = temp->next;
+        }
+
+        return len;
+    }
+
+
+    ListNode* rotateList(ListNode* head){
+        if (head == NULL){
+            return NULL;
+        }
+
+        // 1, 2, 3, 4, 5
+        ListNode* temp = head;
+        while(temp->next->next){
+            temp = temp->next;
+        }
+        // temp as 4
+
+        ListNode* rotatedHead = temp->next;
+        rotatedHead->next = head;
+        temp->next = nullptr;
+
+        return rotatedHead;
+
+    }
+
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+        if(head == NULL){
+            return NULL;
+        }else if(head->next == NULL){
+            return head;
+        }
+
+        long L = length(head);
+        k = k % L;
+
+        if(k == 0){
+            return head;
+        }
+
+        ListNode* rotatedHead = rotateList(head);
+
+        while(k-1){
+            rotatedHead = rotateList(rotatedHead);
+            k--;
+        }
+
+        return rotatedHead;
+    }
+
+*/
