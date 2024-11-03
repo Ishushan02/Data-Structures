4f3469dab14480f934cb5a9f48b2ad5b08f2f021
ishushan02 <ishananand2002@gmail.com>
2024-11-02 19:08:46 -0700

    Stack Recursion Operation for Getting middle element

---
+++ Lecture20/1_stack.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+
+using namespace std;
+
+/*
+    Follows LIFO Ordering(Last In First Out)
+    Stack can only be accessible from TOP and no other way to access it
+
+*/
+
+
+// insert atBottom
+// get MiddleElement
+
+/*
+    So, the biggest advantage is that, we can use recursion for stack oeration as it also follows STACK operation in backend
+*/
+
+void printMiddleElem(stack<int> &stck, int n, int i){
+    if(stck.empty()){
+        return ;
+    }
+    if(n/2 == i){
+        cout <<" Middle Element is " <<stck.top() << endl;
+        return;
+    }
+    int elem = stck.top();
+    stck.pop();
+    i= i + 1;
+    printMiddleElem(stck, n, i);
+    stck.push(elem);
+}
+
+int main(){
+
+    stack<int> s;
+    s.push(10);
+    s.push(5);
+    s.push(11);
+    s.push(13);
+    s.push(9);
+    s.push(12);
+    s.push(8);
+    s.push(14);
+    s.push(20);
+
+    cout << s.size() << endl;
+    cout << " Stack s is empty " << s.empty() << endl;
+    cout << " Top Element " << s.top() << endl;
+
+    // As we know that Stack is used in Recursion operation so, we  take advantage of that.
+
+    printMiddleElem(s, s.size(), 0);
+
+    return 0;
+}
