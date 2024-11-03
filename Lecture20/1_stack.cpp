05de35f1cc311d8915f84fabee12bff3053755dc
ishushan02 <ishananand2002@gmail.com>
2024-11-02 20:38:16 -0700

    Reverse and Insert At Bottom

--- Lecture20/1_stack.cpp
+++ Lecture20/1_stack.cpp
@@ -18,6 +18,15 @@ using namespace std;
     So, the biggest advantage is that, we can use recursion for stack oeration as it also follows STACK operation in backend
 */
 
+void printstack(stack<int>& stck){
+    if(stck.empty()){
+        return;
+    }
+    cout << stck.top() << " ";
+    stck.pop();
+    printstack(stck);
+}
+
 void printMiddleElem(stack<int> &stck, int n, int i){
     if(stck.empty()){
         return ;
@@ -32,6 +41,40 @@ void printMiddleElem(stack<int> &stck, int n, int i){
     printMiddleElem(stck, n, i);
     stck.push(elem);
 }
+
+
+
+void insertAtBottom(stack<int>& stck, int val){
+    if(stck.empty()){
+        stck.push(val);
+        return;
+    }
+
+    int elem = stck.top();
+    stck.pop();
+    insertAtBottom(stck, val);
+
+    // Back Tracking
+    stck.push(elem);
+}
+
+void reverse(stack<int>& stck){
+
+    if(stck.empty()){
+        
+        return;
+    }
+
+    int val = stck.top();
+
+    stck.pop();
+
+    reverse(stck);
+    insertAtBottom(stck, val);
+
+
+}
+
 
 int main(){
 
@@ -52,7 +95,13 @@ int main(){
 
     // As we know that Stack is used in Recursion operation so, we  take advantage of that.
 
-    printMiddleElem(s, s.size(), 0);
+    // printMiddleElem(s, s.size(), 0);
+
+    // insertAtBottom(s, 1000);
+    // printstack(s);
+
+    // reverse(s);
+    // printstack(s);
 
     return 0;
 }
