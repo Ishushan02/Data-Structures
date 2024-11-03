e50c702a9afb774de689566b36b71c79e744eb01
ishushan02 <ishananand2002@gmail.com>
2024-11-03 05:35:15 -0800

    Expression Containing Redundant Brackets

--- Lecture20/1_stack.cpp
+++ Lecture20/1_stack.cpp
@@ -196,3 +196,32 @@ int main(){
 
 
 */
+
+/*
+
+    Important Question
+
+    Smaller on Left
+    (https://www.geeksforgeeks.org/problems/smallest-number-on-left3403/1)
+
+    vector<int> leftSmaller(int n, int a[]){
+        // code here
+        
+        stack<int> st;
+        st.push(-1);
+        
+        vector<int> ans;
+        for(int i = 0; i < n; i++){
+            
+            while(st.top()>=a[i]){
+                st.pop();
+            }
+            ans.push_back(st.top());
+            st.push(a[i]);
+            
+        }
+        
+        return ans;
+    }
+
+*/
