60415f61e47c68d0cc3c464c41ca2848e517a42b
Ishushan02 <ishananand2002@gmail.com>
2024-11-27 06:48:42 -0800

    File Change

---
+++ Lecture20/3_problems.cpp
@@ -0,0 +1,24 @@
+/*
+    739. Daily Temperatures (Good Question )
+    (https://leetcode.com/problems/daily-temperatures/description/)
+
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> st;
+
+        for(int i = 0; i < n; i++){
+
+            while(!st.empty() && temperatures[i] > temperatures[st.top()]){
+                ans[st.top()] = i - st.top();
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+*/
