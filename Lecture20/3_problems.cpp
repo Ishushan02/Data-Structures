df6688385d52105366472045cc9941d82f41ba40
Ishushan02 <ishananand2002@gmail.com>
2024-11-28 06:00:44 -0800

    Remove K Digits

--- Lecture20/3_problems.cpp
+++ Lecture20/3_problems.cpp
@@ -22,3 +22,71 @@
     }
 
 */
+
+
+
+/*
+
+    402. Remove K Digits
+    (https://leetcode.com/problems/remove-k-digits/description/)
+
+    void reversest(stack<char>& st, stack<char>& ansst){
+        if(st.empty()){
+            return;
+        }
+        char topelem = st.top();
+        st.pop();
+        ansst.push(topelem);
+        reversest(st, ansst);
+        
+    }
+
+    string removeKdigits(string num, int k) {
+
+        // if(num.length() ==1 and k == 1){
+        //     return "0";
+        // }
+        string ans;
+        stack <char> st;
+
+        int i = 0;
+        while(i < num.length()){
+            char curr = num[i];
+
+            while(!st.empty() && st.top() > curr && k > 0){
+                st.pop();
+                k--;
+            }
+
+            st.push(curr);
+            i++;
+        }
+
+        // for remaining k's
+        while(!st.empty() && k != 0){
+            st.pop();
+            k--;
+        }
+
+        stack<char> ansst;
+        reversest(st, ansst);
+
+        // removing all leading 0's
+
+        while(!ansst.empty() && ansst.top() == '0'){
+            ansst.pop();
+        }
+
+        if(ansst.empty()){
+            return "0";
+        }
+        
+        while(!ansst.empty()){
+            ans += ansst.top();
+            ansst.pop();
+
+        }
+
+        return ans;
+    }
+*/
