ef7d7fd8e55af9fecf1ce19552c02ea269580b68
Ishushan02 <ishananand2002@gmail.com>
2024-12-01 06:50:50 -0800

    Sliding Window Max Elem

--- Lecture21/1_queue.cpp
+++ Lecture21/1_queue.cpp
@@ -425,3 +425,46 @@ Procedure is (let's say window size is K)
     }
 
 */
+
+
+/*
+
+    239. Sliding Window Maximum (VVIII Question)
+    (https://leetcode.com/problems/sliding-window-maximum/description/)
+
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // always try using deque as we can perform back operation on it
+        deque<int> que;
+        vector<int> ans;
+
+        for(int i = 0; i< k; i++){
+
+            while(!que.empty() && nums[i] > nums[que.back()]){
+                que.pop_back();
+            }
+            que.push_back(i);
+        }
+
+        for(int i = k; i < nums.size(); i++){
+
+            // store answer for prev window
+            ans.push_back(nums[que.front()]);
+
+            // remove if prev window idx exist
+            if(!que.empty() && i - que.front() >= k){
+                que.pop_front();
+            }
+
+            // remove any smaller elem's from the window from back
+            while(!que.empty() && nums[i] > nums[que.back()]){
+                que.pop_back();
+            }
+            que.push_back(i);
+
+        }
+        ans.push_back(nums[que.front()]);
+
+        return ans;
+    }
+
+*/
