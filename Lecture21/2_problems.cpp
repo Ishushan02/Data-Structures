776cfed5e193e9f4736d100ccd8a57cc763ba58e
Ishushan02 <ishananand2002@gmail.com>
2024-12-01 21:41:14 -0800

    Gas Station Method II

--- Lecture21/2_problems.cpp
+++ Lecture21/2_problems.cpp
@@ -31,4 +31,34 @@
         return -1;
     }
 
+
+    // Method 2 -- great TLE 100%
+
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+
+        int balance = 0;
+        int deficiet = 0;
+        int start = 0;
+
+        for(int i = 0; i < gas.size(); i++){
+
+            if(balance + gas[i] < cost[i]){
+                // deficiet case can't go further
+                deficiet += balance + gas[i] - cost[i];
+                start = i + 1;
+                balance = 0;
+            }else{
+                // balance case
+                balance = balance + gas[i] - cost[i];
+            }
+            
+        }
+
+        if(balance + deficiet >= 0 ){
+            return start;
+        }
+
+        return -1;
+    }
+
 */
