fbe612cd0ae885a38898db341d7161d9821f9516
Ishushan02 <ishananand2002@gmail.com>
2024-12-08 17:45:27 -0800

    Stack Using Queue

--- Lecture21/3_problems.cpp
+++ Lecture21/3_problems.cpp
@@ -460,3 +460,36 @@ public:
 */
 
 
+/*
+
+    225. Implement Stack using Queues
+    (https://leetcode.com/problems/implement-stack-using-queues/description/)
+
+        class MyStack {
+public:
+    deque<int> que;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        que.push_back(x);
+    }
+    
+    int pop() {
+        int val = que.back();
+        que.pop_back();
+        return val;
+    }
+    
+    int top() {
+        return que.back();
+    }
+    
+    bool empty() {
+        return que.empty();
+    }
+};
+
+
+*/
