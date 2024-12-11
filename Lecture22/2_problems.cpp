9f06390fceae0794a8f538203ed8ba575900a524
Ishushan02 <ishananand2002@gmail.com>
2024-12-11 08:15:29 -0800

    Diameter of a Tree

--- Lecture22/2_problems.cpp
+++ Lecture22/2_problems.cpp
@@ -27,3 +27,44 @@ using namespace std;
 */
 
 
+/*
+    543. Diameter of Binary Tree
+    (https://leetcode.com/problems/diameter-of-binary-tree/description/)
+
+    int getHeight(TreeNode* root){
+        if(root == NULL){
+            return 0;
+        }
+
+        int leftH = getHeight(root->left);
+        int rightH = getHeight(root->right);
+
+        return max(leftH, rightH) + 1;
+    }
+
+
+    int getDiameter(TreeNode* root){
+        if(root == NULL){
+            return 0;
+        }
+
+        // option1 max diam is present in left part
+        int opt1 = getDiameter(root->left);
+
+        // option2 max height is present in right part
+        int opt2 = getDiameter(root->right);
+
+        // option 3 max diam is present in by combining both the height
+        int opt3 = getHeight(root->left) + getHeight(root->right);
+
+        int maxDiam = max(opt1, max(opt2, opt3));
+
+        return maxDiam;
+
+    }
+
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        return getDiameter(root);
+    }
+*/
