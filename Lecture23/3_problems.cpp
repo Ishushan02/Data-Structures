dcd9df362b1fe45e52667db3ae3a43fc2420cda0
Ishushan02 <ishananand2002@gmail.com>
2024-12-22 06:46:04 -0800

    Reverse BST Itterator

--- Lecture23/3_problems.cpp
+++ Lecture23/3_problems.cpp
@@ -41,3 +41,46 @@
 
 
 */
+
+/*
+    The ABove question is about printing elements in left order (Increasing Order)
+    The current one is about printing the elements in decreasing element. (Decreasing Order)
+    It doesn't have any question just implemented it, for Interview Perspective.
+
+    class BSTIterator {
+public:
+
+    stack<TreeNode*> reverseSt; // reverse Order traversal
+
+
+    void pushRightNode(TreeNode* root){
+        if(root){
+            reverseSt.push(root);
+            root = root->right;
+        }
+    }
+
+    BSTIterator(TreeNode* root) {
+        pushRightNode(root);
+    }
+    
+
+    int before(){
+        TreeNode* topNode = reverseSt.top();
+        reverseSt.pop();
+
+        if(topNode->left){
+            pushRightNode(topNode);
+        }
+        return topNode->val;
+    }
+
+    bool hasBefore(){
+        if(reverseSt.empty()){
+            return false;
+        }
+        return true;
+    }
+    
+};
+*/
