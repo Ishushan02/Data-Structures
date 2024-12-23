c2814fd5cb67b935a2a5a7e4056ee59bda6e3650
Ishushan02 <ishananand2002@gmail.com>
2024-12-23 08:01:50 -0800

    Median of BST

--- Lecture23/3_problems.cpp
+++ Lecture23/3_problems.cpp
@@ -629,3 +629,40 @@ public:
     }
 
 */
+
+/*
+    Median of BST
+    (https://www.geeksforgeeks.org/problems/median-of-bst/0)
+
+    void getInorder(Node* root, vector<int> &arr){
+    if(root == NULL){
+        return ;
+    }
+    
+    getInorder(root->left, arr);
+    arr.push_back(root->data);
+    getInorder(root->right, arr);
+}
+
+float findMedian(struct Node *root)
+{
+      //Code here
+      vector<int> arr;
+      getInorder(root, arr);
+      
+      int n = arr.size();
+      if(n%2 != 0){
+          int index = n/2 ;
+          return arr[index];
+      }else{
+          
+          float id1 = arr[n/2];
+          float id2 = arr[n/2 - 1];
+        //   cout << arr[id1] << "  " << arr[id2] << endl;
+          return (id1 + id2)/2;
+      }
+      
+      return 0.0;
+}
+
+*/
