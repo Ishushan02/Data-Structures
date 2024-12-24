0f22d1e046d4ee9449e820484b3c2e89996bdf9d
Ishushan02 <ishananand2002@gmail.com>
2024-12-23 20:17:20 -0800

    Diam of BN

--- NeetCode150/1_problems.cpp
+++ NeetCode150/1_problems.cpp
@@ -1,4 +1,9 @@
 /*
+    std::map::lower_bound() is a built-in method used to find the first element 
+    in the map whose key is either equal to or greater than the given key. 
+    In this article, we will learn about std::map::lower_bound() function in C++.
+
+
     981. Time Based Key-Value Store {I didn't know lower_bound function, remember it.. focus on it}
     (https://leetcode.com/problems/time-based-key-value-store/)
 
@@ -41,5 +46,32 @@ public:
         return "";
     }
 };
+
+*/
+
+
+/*
+    543. Diameter of Binary Tree
+    (https://leetcode.com/problems/diameter-of-binary-tree/description/)
+
+    int diam(TreeNode* root, int &diameter){
+        if(root == NULL){
+            return 0;
+        }
+
+        int lh = diam(root->left, diameter);
+        int rh = diam(root->right, diameter);
+
+        diameter = max(diameter, lh + rh);
+
+        return max(lh, rh) + 1;
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        int x = diam(root, diameter);
+
+        return diameter;
+    }
 
 */
