a31f06b9b14533067e74518d21fb4a6ac82d13f4
Ishushan02 <ishananand2002@gmail.com>
2025-01-02 19:23:48 -0800

    NeetCode: Permutation

--- NeetCode150/1_problems.cpp
+++ NeetCode150/1_problems.cpp
@@ -115,7 +115,7 @@ public:
 /*
     90. Subsets II
     (https://leetcode.com/problems/subsets-ii/)
-    
+
     void getSubset(vector<int>& nums, int index, vector<int> tempAns, vector<vector<int>> &res ){
         if(index >= nums.size()){
             if(find(res.begin(), res.end(), tempAns) == res.end()){
@@ -140,4 +140,32 @@ public:
 
         return res;
     }
+*/
+
+
+/*
+    46. Permutations
+    (https://leetcode.com/problems/permutations/)
+
+    void permutation(vector<int>& nums, vector<vector<int>> &ans, int index){
+        if(index >= nums.size()){
+            ans.push_back(nums);
+            return ;
+        }
+
+        for(int i = index; i<nums.size(); i++){
+            swap(nums[i], nums[index]);
+            permutation(nums, ans, index + 1);
+            swap(nums[i], nums[index]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        permutation(nums, ans, 0);
+
+        return ans;
+    }
+
+
 */
