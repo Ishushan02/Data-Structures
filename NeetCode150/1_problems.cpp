38a1fbc0a47008a92b87789ac3e7c7ea8446d641
Ishushan02 <ishananand2002@gmail.com>
2025-01-07 08:30:22 -0800

    NeetCode: 338. Counting Bits

--- NeetCode150/1_problems.cpp
+++ NeetCode150/1_problems.cpp
@@ -279,3 +279,26 @@ public:
     }
     
 */
+
+/*
+    338. Counting Bits
+    (https://leetcode.com/problems/counting-bits/)
+
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i = 0; i<= n; i++){
+            int count = 0;
+            int temp = i;
+            while(temp){
+                if(temp & 1){
+                    count += 1;
+                }
+                temp = temp >> 1;
+            }
+            ans.push_back(count);
+        }
+
+        return ans;
+    }
+    
+*/
