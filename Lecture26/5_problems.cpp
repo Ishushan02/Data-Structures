44c3156e6b4587d2b6ad10355f3e773251a9b3e2
Ishushan02 <ishananand2002@gmail.com>
2025-01-11 08:16:03 -0800

    Guess Higher or Lower

--- Lecture26/5_problems.cpp
+++ Lecture26/5_problems.cpp
@@ -3,3 +3,56 @@
 PATTERN - MERGE INTERVALS
 
 */
+
+/*
+    375. Guess Number Higher or Lower II
+    (https://leetcode.com/problems/guess-number-higher-or-lower-ii/description/)
+
+    int computeCost(int start, int end){
+        if(start >= end){
+            return 0;
+        }
+
+        int ans = INT_MAX;
+        for(int i = start; i<end; i++){
+            int lcost = i + computeCost(start, i - 1);
+            int rcost = i + computeCost(i + 1, end);
+
+            int maxCost = max(lcost, rcost);
+            ans = min(ans, maxCost);
+        }
+
+        return ans;
+    }
+
+    int computeCostDP(int start, int end, vector<vector<int>> &dpArr){
+        if(start >= end){
+            return 0;
+        }
+
+        if(dpArr[start][end] != -1){
+            return dpArr[start][end];
+        }
+
+        int ans = INT_MAX;
+        for(int i = start; i<end; i++){
+            int lcost = i + computeCostDP(start, i - 1, dpArr);
+            int rcost = i + computeCostDP(i + 1, end, dpArr);
+
+            int maxCost = max(lcost, rcost);
+            ans = min(ans, maxCost);
+        }
+
+        dpArr[start][end] = ans;
+
+        return dpArr[start][end];
+    }
+
+    int getMoneyAmount(int n) {
+        
+        vector<vector<int>> dpArr(n+1, vector<int>(n+1, -1));
+        // return computeCost(1, n);
+        return computeCostDP(1, n, dpArr);
+    }
+
+*/
