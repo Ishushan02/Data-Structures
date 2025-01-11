0cccc762a02b82244ee7fc50994304527761a676
Ishushan02 <ishananand2002@gmail.com>
2025-01-11 05:38:24 -0800

    DP: Maximum Height by Stacking Cuboids

--- Lecture26/4_problems.cpp
+++ Lecture26/4_problems.cpp
@@ -52,3 +52,85 @@
     }
 
 */
+
+
+/*  
+    1691. Maximum Height by Stacking Cuboids
+    (https://leetcode.com/problems/maximum-height-by-stacking-cuboids/description/)
+
+
+    // your thought was area, but why make it complex just compare there l,h,w
+
+    bool isSafe(vector<int>& cube1, vector<int>& cube2){
+        if((cube1[0] <= cube2[0]) && (cube1[1] <= cube2[1]) && (cube1[2] <= cube2[2])){
+            return true;
+        }
+
+        return false;
+    }
+
+
+    int getMaxCuboidsHeight(vector<vector<int>>& cuboids,int prevIdx, int index){
+
+        if(index >= cuboids.size()){
+            return 0;
+        }
+
+        int include = 0;
+        int exclude = 0;
+        // include
+        if(prevIdx== -1 || isSafe(cuboids[prevIdx], cuboids[index])){
+            include = cuboids[index][2] + getMaxCuboidsHeight(cuboids, index, index + 1);
+        }
+
+        // exclude
+        exclude = 0 + getMaxCuboidsHeight(cuboids, prevIdx, index + 1);
+
+        int height = max(include, exclude);
+
+        return height;
+    }
+
+
+    int getMaxCuboidsHeightDP(vector<vector<int>>& cuboids,int prevIdx, int index, vector<vector<int>> &dpArr){
+
+        if(index >= cuboids.size()){
+            return 0;
+        }
+
+        if(dpArr[index][prevIdx+1] != -1){
+            return dpArr[index][prevIdx+1];
+        }
+
+        int include = 0;
+        int exclude = 0;
+        // include
+        if(prevIdx== -1 || isSafe(cuboids[prevIdx], cuboids[index])){
+            include = cuboids[index][2] + getMaxCuboidsHeightDP(cuboids, index, index + 1, dpArr);
+        }
+
+        // exclude
+        exclude = 0 + getMaxCuboidsHeightDP(cuboids, prevIdx, index + 1, dpArr);
+
+        dpArr[index][prevIdx+1] = max(include, exclude);
+
+        return dpArr[index][prevIdx+1];
+    }
+
+    int maxHeight(vector<vector<int>>& cuboids) {
+        
+        // first sort all cuboids internally it will take O(1) time
+
+        for(int i = 0; i < cuboids.size(); i++){
+            sort(cuboids[i].begin(), cuboids[i].end());
+        }
+
+        // now we sort all cuboids based on there first elements
+        sort(cuboids.begin(), cuboids.end());
+        int n = cuboids.size();
+        vector<vector<int>> dpArr(n+1, vector<int>(n + 1, -1));
+        // return getMaxCuboidsHeight(cuboids, -1, 0);
+        return getMaxCuboidsHeightDP(cuboids, -1, 0, dpArr);
+    }
+
+*/
