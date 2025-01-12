0a262e0636960c9b448e48bb43a68b8129dca8d7
Ishushan02 <ishananand2002@gmail.com>
2025-01-12 06:08:58 -0800

    DP: WildCard Matching

---
+++ Lecture26/6_problems.cpp
@@ -0,0 +1,98 @@
+/*
+    44. Wildcard Matching
+    (https://leetcode.com/problems/wildcard-matching/description/)
+
+    bool stringMatch(string &s, string &p, int i, int j){
+
+        if(i == s.length() && j == p.length()){
+            return true;
+        }
+
+        if(j >= p.length()){
+            return false;
+        }
+
+        if(i == s.length()){
+            while(j < p.length()){
+                if(p[j] != '*'){
+                    return false;
+                }
+                j++;
+            }
+            return true;
+        }
+        
+        bool ans = true;
+        if(s[i] == p[j] || p[j] == '?'){
+            ans  = true && stringMatch(s, p, i+1, j+1);
+        }else if(p[j] == '*'){
+            // once make * as empty string
+            // and then make it as same as current string
+            bool matchans = stringMatch(s, p, i+1, j) || stringMatch(s, p, i, j+1);
+            ans = ans && matchans;
+        }else{
+            // cout << " Here-- " << endl;
+            return false;
+        }
+
+        return ans;
+
+    }
+
+    bool stringMatchDP(string &s, string &p, int i, int j, vector<vector<int>> &dpArr){
+
+        if(i == s.length() && j == p.length()){
+            return true;
+        }
+
+        if(j >= p.length()){
+            return false;
+        }
+
+        if(i == s.length()){
+            while(j < p.length()){
+                if(p[j] != '*'){
+                    return false;
+                }
+                j++;
+            }
+            return true;
+        }
+
+        if(dpArr[i][j] != -1){
+            return dpArr[i][j];
+        }
+        
+        bool ans = true;
+        if(s[i] == p[j] || p[j] == '?'){
+            ans  = true && stringMatchDP(s, p, i+1, j+1, dpArr);
+        }else if(p[j] == '*'){
+            // once make * as empty string
+            // and then make it as same as current string
+            bool matchans = stringMatchDP(s, p, i+1, j, dpArr) || stringMatchDP(s, p, i, j+1, dpArr);
+            ans = ans && matchans;
+        }else{
+            // cout << " Here-- " << endl;
+            return false;
+        }
+
+        if(ans == true){
+            dpArr[i][j] = 1;
+        }else{
+            dpArr[i][j] = 0;
+        }
+        
+
+        return dpArr[i][j];
+
+    }
+
+    bool isMatch(string s, string p) {
+        // return stringMatch(s, p, 0, 0);
+        int m = s.length();
+        int n = p.length();
+        // -1 nothing, +1 true, 0, false;
+        vector<vector<int>> dpArr(m+1, vector<int>(n+1, -1));
+        return stringMatchDP(s, p, 0, 0, dpArr);
+    }
+*/
