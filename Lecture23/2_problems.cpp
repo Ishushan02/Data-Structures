440bdcdc7852b11b09a8e71f0d75a80c53c48da6
Ishushan02 <ishananand2002@gmail.com>
2025-01-20 16:31:10 -0800

    Generate Tree

--- Lecture23/2_problems.cpp
+++ Lecture23/2_problems.cpp
@@ -474,6 +474,33 @@
 
         return root;
     }
+
+
+
+    Method 2
+
+    TreeNode* generateTree(vector<int>& nums, int start, int end){
+
+        if(start > end){
+            return NULL;
+        }
+
+        int mid = (start + end)/2;
+
+        TreeNode* root = new TreeNode(nums[mid]);
+        
+        root->left = generateTree(nums, start, mid - 1);
+        root->right = generateTree(nums, mid+1, end);
+
+        return root;
+
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        
+        return generateTree(nums, 0, nums.size() -1);
+    }
+
 */
 
 /*
