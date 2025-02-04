cf72073c30ae164edc518bb6b8a7b7605bfb8aaa
Ishushan02 <ishananand2002@gmail.com>
2025-02-04 15:19:48 -0800

    Graph : Kruskal's Algorithm

--- Lecture27/7_problems.cpp
+++ Lecture27/7_problems.cpp
@@ -73,3 +73,64 @@ So, get the minimum cost out of all the possible Spanning Trees, that's MST
         return totalCost;
     }
 */
+
+
+/*
+    // Don't run this in GFG because both Kruskals and Prism might give different Answers at times
+    Kruskal's Algorithm Implementation
+
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        // code here
+        
+        // {weight, {node1, node2}}
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>> > que;
+        
+        for (int i = 0; i < V; i++) {
+            for (auto v : adj[i]) {
+                // adjList[i].push_back({v[1], v[0]});
+                que.push({v[1], {i, v[0]}});
+                // que.push({v[1], {v[0], i}});
+                // cout << v[0] << "," << v[1]  <<endl;
+            }
+        }
+        
+        int visitedCount = 0;
+        vector<int> visited(V, 0);
+        int totalCost = 0;
+        
+        while(visitedCount < V){
+            pair<int, pair<int, int>> topedge = que.top();
+            int wt = topedge.first;
+            int node1 = topedge.second.first;
+            int node2 = topedge.second.second;
+            
+            if(visited[node1] == 1 && visited[node2] == 1){
+                que.pop();
+                continue;
+            }
+            totalCost += wt;
+            que.pop();
+            
+            // cout << node1 << ", "<< node2 <<" ->"  << wt << endl;
+            
+            if(visited[node1] == 0){
+                // cout << node1 << endl;
+                visitedCount++;
+                visited[node1] = 1;
+            }
+            
+            if(visited[node2] == 0){
+                // cout << node2 << endl;
+                visitedCount++;
+                visited[node2] = 1;
+            }
+
+        }
+        
+        return totalCost;
+        
+        
+        
+    }
+
+*/
