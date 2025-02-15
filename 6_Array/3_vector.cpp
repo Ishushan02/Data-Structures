5cbb1a4bce29863182c7f19b6a7011d643a1e3eb
Ishushan02 <ishananand2002@gmail.com>
2025-02-14 18:20:43 -0800

    Rotate Image

--- 6_Array/3_vector.cpp
+++ 6_Array/3_vector.cpp
@@ -4,3 +4,35 @@ using namespace std;
 int main(){
     
 }
+
+
+/*
+    48. Rotate Image
+    (https://leetcode.com/problems/rotate-image/description/)
+
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        int i = 0;
+        int j = n - 1;
+
+        while(i < j){
+
+            for(int row = 0; row < n; row++){
+                swap(matrix[row][i], matrix[row][j]);
+            }
+            i++;
+            j--;
+        }
+
+
+    }
+        
+*/
