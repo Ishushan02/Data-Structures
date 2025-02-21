40c5b7ceb7f10cd484bd8229587a50e84f3ea24e
Ishushan02 <ishananand2002@gmail.com>
2025-02-21 12:06:04 -0800

    Stack: Min String after removing SubStr

--- 16_Stacks/4_miscellenous.cpp
+++ 16_Stacks/4_miscellenous.cpp
@@ -28,5 +28,34 @@
 
         return ans;
     }
+
+*/
+
+/*
+    2696. Minimum String Length After Removing Substrings
+    (https://leetcode.com/problems/minimum-string-length-after-removing-substrings/description/)
+
+    int minLength(string s) {
+        
+        stack<char> st;
+        int i = 0;
+
+        while(i < s.length()){
+            
+            if(s[i] == 'B' && !st.empty() && st.top() == 'A'){
+                st.pop(); // AB
+            }else if(s[i] == 'D' && !st.empty() && st.top() == 'C'){
+                st.pop(); // CD
+            }else{
+                st.push(s[i]);
+            }
+            i++;
+            
+        }
+
+        
+
+        return st.size();
+    }
         
 */
