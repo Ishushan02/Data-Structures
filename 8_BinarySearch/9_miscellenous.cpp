ef490a785569362d32ad7d9f95f2ba2371063165
Ishushan02 <ishananand2002@gmail.com>
2025-03-11 11:18:11 -0700

    Binary Search: K Weakest Rows in a Mtrix

---
+++ 8_BinarySearch/9_miscellenous.cpp
@@ -0,0 +1,69 @@
+/*
+    374. Guess Number Higher or Lower
+    (https://leetcode.com/problems/guess-number-higher-or-lower/description/)
+
+    int guessNumber(int n) {
+        
+        int start = 1;
+        int end = n;
+        int mid = start+((end-start)/2);
+
+        while (start <= end){
+            int val = guess(mid);
+
+            if (val == -1){
+                end = mid-1;
+            }else if (val == 1){
+                start = mid+1;
+            }else{
+                return mid;
+            }
+            mid = start+((end-start)/2);
+        }
+        return n;
+    }
+
+*/
+
+/*
+    1337. The K Weakest Rows in a Matrix
+    (https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/description/)
+
+    struct comp{
+        bool operator()(pair<int, int> &a, pair<int, int> &b){
+            if(a.second == b.second){
+                if(a.first < b.first){
+                    return true;
+                }
+                return false;
+            }else if(a.second < b.second){
+                return true;
+            }
+            return false;
+        }
+    };
+
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        vector<pair<int, int>> valPair;
+
+        for(int i = 0; i < mat.size(); i++){
+            int col = mat[0].size();
+            int count = 0;
+            for(auto v : mat[i]){
+                if(v == 1){
+                    count++;
+                }
+            }
+            valPair.push_back({i, count});
+        }
+        sort(valPair.begin(), valPair.end(), comp());
+        vector<int> ans;
+
+        for(int i = 0; i < k; i++){
+            ans.push_back(valPair[i].first);
+        }
+
+        return ans;
+    }
+        
+*/
