b7f1b6eea3ad17eb84f66e854c0d747acb10b14e
Ishushan02 <ishananand2002@gmail.com>
2025-03-22 05:14:51 -0700

    DP: Digit DP

---
+++ 22_DynamicProgramming/12_digitDP.cpp
@@ -0,0 +1,13 @@
+/*
+DO THIS CONCEPT LATER, IT IS IMPORTANT....
+*/
+
+/*
+Digit DP, variations using digits and getting some sort of relations
+
+    Type 1 - give the count of X where 0 <= X <= R; that follows f(x)
+    Type 2 - The count of values X which is in range of L <= X <= R; that follows f(x)
+
+    where f(x) is a binary function
+    
+*/
