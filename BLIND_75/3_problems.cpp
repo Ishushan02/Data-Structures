506dc12fbb623faace4459356c50bd45e10cb6e2
Ishushan02 <ishananand2002@gmail.com>
2025-03-29 16:12:24 -0700

    Blind 75: Counting Bits

--- BLIND_75/3_problems.cpp
+++ BLIND_75/3_problems.cpp
@@ -369,5 +369,29 @@
         return false;
 
     }
+
+*/
+
+/*
+    Problem 41
+    338. Counting Bits
+    (https://leetcode.com/problems/counting-bits/)
+
+    vector<int> countBits(int n) {
         
+        vector<int> ans;
+
+        for(int i = 0; i <= n; i++){
+            int count = 0;
+            int k = i;
+            while(k){
+                if(k & 1)
+                    count++;
+                k = k >> 1;
+            }
+            ans.push_back(count);
+        }
+
+        return ans;
+    }
 */
