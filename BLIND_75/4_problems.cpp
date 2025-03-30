fc0a82265c4824c995c084bafaf5f3b1624f1d42
Ishushan02 <ishananand2002@gmail.com>
2025-03-30 16:59:35 -0700

    Blind 75: Maximum Depth of BT

--- BLIND_75/4_problems.cpp
+++ BLIND_75/4_problems.cpp
@@ -180,5 +180,26 @@
         getElements(root, ans);
         return ans[k-1];
     }
+
+*/
+
+/*
+    Problem 52
+    104. Maximum Depth of Binary Tree
+    (https://leetcode.com/problems/maximum-depth-of-binary-tree/)
+
+    int height(TreeNode* root){
+        if(root == NULL){
+            return 0;
+        }
+
+        int l = height(root->left);
+        int r = height(root->right);
+        return max(l, r) + 1;
+    }
+
+    int maxDepth(TreeNode* root) {
+        return height(root);
+    }
         
 */
