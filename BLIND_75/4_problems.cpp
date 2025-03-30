7b1e5dde475e6dc2fb024b359890dfc1dfa5993f
Ishushan02 <ishananand2002@gmail.com>
2025-03-30 15:01:09 -0700

    Blind 75:Validate Binary Search Tree

--- BLIND_75/4_problems.cpp
+++ BLIND_75/4_problems.cpp
@@ -29,3 +29,27 @@
 
     }
 */
+
+/*
+    Problem 47
+    98. Validate Binary Search Tree
+    (https://leetcode.com/problems/validate-binary-search-tree/)
+
+    bool isBST(TreeNode* root, long minVal, long maxVal){
+        if(root == NULL){
+            return true;
+        }
+
+        if(root->val <= minVal || root->val >= maxVal){
+            return false;
+        }
+
+        return isBST(root->left, minVal, root->val) && isBST(root->right, root->val, maxVal);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        
+        return isBST(root->left, LONG_MIN, root->val) && isBST(root->right, root->val, LONG_MAX);
+    }
+        
+*/
