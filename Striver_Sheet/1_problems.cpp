720ded60bcebcc6e8bbb221df616eac6f9536f04
Ishushan02 <ishananand2002@gmail.com>
2025-04-03 06:47:39 -0700

    StriverSheet - Remove Duplicates from Sorted Array

--- Striver_Sheet/1_problems.cpp
+++ Striver_Sheet/1_problems.cpp
@@ -805,5 +805,31 @@
         return ansHead->next;
 
     }
+
+*/
+
+/*
+    Problem 16
+    26. Remove Duplicates from Sorted Array
+    (https://leetcode.com/problems/remove-duplicates-from-sorted-array/)
+
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        int j = 1;
+
+        while(j < n){
+
+            if(nums[i] == nums[j]){
+                j++;
+            }else{
+                i++;
+                nums[i] = nums[j];
+                j++;
+            }
+        }
         
+
+        return i+1;
+    }
 */
