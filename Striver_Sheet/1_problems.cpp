50964f52770ce792895d312dce92db3acf696a58
Ishushan02 <ishananand2002@gmail.com>
2025-04-03 17:42:15 -0700

    StriverSheet - Diameter of BT

--- Striver_Sheet/1_problems.cpp
+++ Striver_Sheet/1_problems.cpp
@@ -901,5 +901,42 @@
 
         return -1;
     }
+
+*/
+
+/*  
+    Problem 19
+    543. Diameter of Binary Tree
+    (https://leetcode.com/problems/diameter-of-binary-tree/)
+
+    int height(TreeNode* root){
+        if(root == NULL){
+            return 0;
+        }
+
+        int lh = height(root->left);
+        int rh = height(root->right);
+
+        return max(lh, rh) + 1;
+    }
+
+    void diameter(TreeNode* root, int &ans){
+        if(root == NULL){
+            return ;
+        }
+        
+        int diam = height(root->left) + height(root->right);
+        ans = max(ans, diam);
+
+        diameter(root->left, ans);
+        diameter(root->right, ans);
+
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans = 0;
+        diameter(root, ans);
+        return ans;
+    }
         
 */
