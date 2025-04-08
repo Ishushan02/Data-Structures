f2b7a200288e7108c6d6de77b35671a190e25785
Ishushan02 <ishananand2002@gmail.com>
2025-04-08 05:35:23 -0700

    Recursion: Permutation Sequence

--- 13_Recursion/5_problems.cpp
+++ 13_Recursion/5_problems.cpp
@@ -49,3 +49,47 @@
     }
 
 */
+
+/*
+    VVVIII Question (Convert in to blocks, get the index, check if it's 0 or not, and then again update k.. untill n == 1)
+    60. Permutation Sequence
+    (https://leetcode.com/problems/permutation-sequence/)
+
+    int factorial(int n){
+        if(n == 1 || n == 0){
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+    
+    void permuteStringSeq(int n, int k, string &ans, string &s){
+        if(n == 1){
+            ans += s[0];
+            return ;
+        }
+
+        int fct = factorial(n-1);
+        int idx = k / fct;
+        if(k % fct == 0){
+            idx--;
+        }
+
+        ans += s[idx];
+        s.erase(idx, 1);
+        k = k - (idx * fct);
+        permuteStringSeq(n-1, k, ans, s);
+    }
+
+    string getPermutation(int n, int k) {
+
+        string s;
+        for(auto i = '1'; i <= n + '0'; i++){
+            s += i;
+        }
+        cout << s << endl;
+        string ans;
+        permuteStringSeq(n, k, ans, s);
+        return ans;
+    }
+
+*/
