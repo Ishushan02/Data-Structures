a93d8acb36671a91a73a24892173a403fa5eed38
Ishushan02 <ishananand2002@gmail.com>
2025-05-28 18:44:44 -0700

    Problem 42: Same Tree

--- Striver_Sheet/2_problems.cpp
+++ Striver_Sheet/2_problems.cpp
@@ -693,3 +693,25 @@
         return checkValidity(root, LONG_MIN, LONG_MAX);
     }
 */
+
+/*
+
+    Problem 42
+    100. Same Tree
+    (https://leetcode.com/problems/same-tree/)
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL){
+            return true;
+        }else if(p != NULL && q == NULL){
+            return false;
+        }else if(p == NULL && q != NULL){
+            return false;
+        }else if(p->val != q->val){
+            return false;
+        }
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }  
+        
+*/
