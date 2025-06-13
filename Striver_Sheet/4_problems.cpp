1155a09b928c4466914181e7e7301ee086e6920d
Ishushan02 <ishananand2002@gmail.com>
2025-06-13 14:40:12 -0700

    Striver SHeet: Problem 78 LCA in BST

--- Striver_Sheet/4_problems.cpp
+++ Striver_Sheet/4_problems.cpp
@@ -61,3 +61,47 @@
     }
         
 */
+
+/*
+
+    Problem 78
+    (https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/)
+    235. Lowest Common Ancestor of a Binary Search Tree
+
+
+    TreeNode* getParent(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root == NULL){
+            return NULL;
+        }
+
+        if(root->val == p->val){
+            return p;
+        }
+
+        if(root->val == q->val){
+            return q;
+        }
+
+        TreeNode* ans = NULL;
+        if(p->val < root->val && q->val < root->val ){
+            // left
+            return getParent(root->left, p, q);
+        }else if(p->val > root->val && q->val > root->val ){
+            // right
+            return getParent(root->right, p, q);
+
+        }
+        
+        return root;
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        unordered_map<TreeNode*, TreeNode*> nodeMap;
+        if(root == NULL){
+            return NULL;
+        }
+
+        return getParent(root, p, q);
+
+    }
+*/
