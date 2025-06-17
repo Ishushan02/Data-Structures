28e25f6ea9075bcf1f58ee2c44eb819a8f6d0591
Ishushan02 <ishananand2002@gmail.com>
2025-06-17 05:40:00 -0700

    Striver SHeet: Queue from Stack Problem 86

--- Striver_Sheet/4_problems.cpp
+++ Striver_Sheet/4_problems.cpp
@@ -455,3 +455,55 @@
         return ans;
     }
 */
+
+/*
+    Problem 86
+    (https://leetcode.com/problems/implement-queue-using-stacks/)
+    232. Implement Queue using Stacks
+
+    class MyQueue {
+    public:
+        stack<int> st1;
+        stack<int> st2;
+        MyQueue() {
+            
+        }
+        
+        void push(int x) {
+            st1.push(x);
+        }
+        
+        int pop() {
+            while(st1.size()>1){
+                st2.push(st1.top());
+                st1.pop();
+            }
+            int elem = st1.top();
+            st1.pop();
+
+            while(!st2.empty()){
+                st1.push(st2.top());
+                st2.pop();
+            }
+            return elem;
+        }
+        
+        int peek() {
+            while(st1.size()>1){
+                st2.push(st1.top());
+                st1.pop();
+            }
+            int elem = st1.top();
+
+            while(!st2.empty()){
+                st1.push(st2.top());
+                st2.pop();
+            }
+            return elem;
+        }
+        
+        bool empty() {
+            return st1.empty();
+        }
+    };
+*/
