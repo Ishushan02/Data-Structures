85d14b3fc7a965f28f93aeaeb5f20eb3ca6523b4
Ishushan02 <ishananand2002@gmail.com>
2025-06-26 07:22:01 -0700

    Weekly Contest 400: Q2

--- 25_Contests/1_problems.cpp
+++ 25_Contests/1_problems.cpp
@@ -25,6 +25,49 @@ Weekly Contest 400
     }
 
 
+    3169. Count Days Without Meetings
+    struct sortFn{
+        bool operator()(vector<int>& a, vector<int> &b){
+            if(a[0] < b[0]){
+                return true;
+            }else{
+                if(a[0] == b[0]){
+                    return a[1] <= b[1];
+                }
+                return false;
+            }
+        }
+    };
+    
+    int countDays(int days, vector<vector<int>>& meetings) {
+    
+        sort(meetings.begin(), meetings.end(), sortFn());
+        int freeDays = 0;
+        int endTime = meetings[0][1];
+        freeDays = 0;
+        // cout << freeDays << endl;
+        for(int i = 1; i < meetings.size(); i++){
+            int initial = meetings[i][0];
+            meetings[i][0] = max(meetings[i][0], endTime);
+            endTime = max(endTime, meetings[i][1]);
+            // cout << meetings[i][0] << " , " << meetings[i][1] << endl;
+        }
 
+        int prev = -1;
+        for(int i = 0; i < meetings.size(); i++){
+            // cout << meetings[i][0] << " , " << meetings[i][1] << endl;
+            if(meetings[i][1] >= meetings[i][0] ){
+                if(meetings[i][0] > prev){
+                    freeDays += (meetings[i][1] - meetings[i][0] + 1);
+                }else {
+                    freeDays += (meetings[i][1] - meetings[i][0]);
+                }
+                prev = meetings[i][1];
+            }
+            
+        }
+        cout << freeDays << endl;
+        return days - freeDays;
+    }©leetcode
 
 */
