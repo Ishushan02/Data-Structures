347acb477bd4f4c34c29c76fa43469ee6a0194bf
Ishushan02 <ishananand2002@gmail.com>
2025-07-01 06:15:57 -0700

    DP Series - Question - 1

--- 25_DPSeries/1_problems.cpp
+++ 25_DPSeries/1_problems.cpp
@@ -19,3 +19,43 @@
         return curr;
     }
 */
+
+
+/*
+    70. Climbing Stairs
+    (https://leetcode.com/problems/climbing-stairs/description/)
+
+    Tabular DP
+    int climbStairs(int n) {
+        if(n == 1){
+            return 1;
+        }
+        vector<int>Fn(n+1, 0);
+        Fn[0] = 1;
+        Fn[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            Fn[i] = Fn[i-2] + Fn[i-1];
+        }
+        
+        return Fn[n];
+    }
+
+    Space Optimization
+    int climbStairs(int n) {
+        if(n == 1){
+            return 1;
+        }
+        int prev = 1;
+        int curr = 1;
+
+        for(int i = 2; i <= n; i++){
+            int sum = prev + curr;
+            prev = curr;
+            curr = sum;
+        }
+        
+        return curr;
+    }
+
+*/
