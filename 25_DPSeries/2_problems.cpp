c4067469cfe8cf5e6288fc90c8a48b6f61736ce6
Ishushan02 <ishananand2002@gmail.com>
2025-07-03 09:02:30 -0700

    Min Triangle Sum - Space Optimized

--- 25_DPSeries/2_problems.cpp
+++ 25_DPSeries/2_problems.cpp
@@ -18,4 +18,23 @@
         return dpArray[0][0];
     }
 
+    Space Optimized
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+        int n = triangle.size();
+        vector<int> linearDP(n + 1, 0);
+
+        for(int i = m-1; i >= 0; i--){
+            vector<int> temp(n + 1, 0);
+            for(int j = n - 1; j >= 0; j--){
+                if(i >= j){
+                    int sum = min(triangle[i][j] + linearDP[j], triangle[i][j] + linearDP[j+1]);
+                    temp[j] = sum;
+                }
+            }
+            linearDP = temp;
+        }
+        return linearDP[0];
+    }
+
 */
