dcf4f8f50e94df803423a80d9450e3cc05ad92b6
Ishushan02 <ishananand2002@gmail.com>
2025-07-03 08:53:04 -0700

    Min Triangle Sum - OPtimized SOlution

---
+++ 25_DPSeries/2_problems.cpp
@@ -0,0 +1,21 @@
+/*
+    120. Triangle
+    (https://leetcode.com/problems/triangle/description/)
+
+    OPtimized SOlution
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+        int n = triangle.size();
+
+        vector<vector<int>> dpArray(m+1, vector<int>(n+1, 0));
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(i >= j){
+                    dpArray[i][j] = min(triangle[i][j] + dpArray[i+1][j], triangle[i][j] + dpArray[i+1][j+1] );
+                }
+            }
+        }
+        return dpArray[0][0];
+    }
+
+*/
