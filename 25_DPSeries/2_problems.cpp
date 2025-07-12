649b7dafdfed3a136eccc75833575b2f1cf9e13d
Ishushan02 <ishananand2002@gmail.com>
2025-07-12 06:42:41 -0700

    0-1 Knapsack problem Optimized

--- 25_DPSeries/2_problems.cpp
+++ 25_DPSeries/2_problems.cpp
@@ -241,3 +241,30 @@
         return dpArray[0][k] % 1000000007;
     }
 */
+
+/*
+    0 - 1 Knapsack Problem
+    (https://www.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1)
+
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        int n = val.size();
+        
+        vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+
+
+        for (int i = 1; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                int include = 0;
+                if (wt[i - 1] <= w) {
+                    include = max(dp[i][w], dp[i - 1][w - wt[i - 1]] + val[i - 1]);
+                }
+                int exclude = dp[i - 1][w];
+                dp[i][w] = max(include, exclude);
+            }
+        }
+    
+        return dp[n][W];
+        
+    }
+        
+*/
