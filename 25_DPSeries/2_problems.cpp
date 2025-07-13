a8eda7aefce6be9d403ca3268ff417cadd301aaa
Ishushan02 <ishananand2002@gmail.com>
2025-07-12 18:40:48 -0700

    Coin Change II - SPACE OPTIMIZED

--- 25_DPSeries/2_problems.cpp
+++ 25_DPSeries/2_problems.cpp
@@ -370,4 +370,28 @@
 
         return dpArray[0][amount];
     }
+
+
+    SPACE OPTIMIZED
+
+    int change(int amount, vector<int>& coins) {
+        int count = 0;
+        int n = coins.size();
+        vector<long long int> dpCount(amount + 1, 0);
+    
+
+        dpCount[0] = 1;
+
+        for(int i = n -1; i >= 0; i--){
+            for(int j = 1; j <= amount; j++){
+                if(j >= coins[i]){
+                    dpCount[j] += (dpCount[j-coins[i]] % INT_MAX);
+                }
+            }
+        }
+
+
+        return dpCount[amount];
+    }
+        
 */
