6e01b645df0bfde1041ffbf1addc7e489747aff0
Ishushan02 <ishananand2002@gmail.com>
2025-07-24 04:53:15 -0700

    DP Series: Buy n Sell Stock III

--- 25_DPSeries/4_problems.cpp
+++ 25_DPSeries/4_problems.cpp
@@ -118,3 +118,29 @@
     }
         
 */
+
+/*
+
+    123. Best Time to Buy and Sell Stock III
+    (https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/description/)
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> curr(2, vector<int>(3, 0));
+        vector<vector<int>> next(2, vector<int>(3, 0));
+        
+        for(int i = n-1; i >= 0; i--){
+            for(int j = 1; j >= 0; j--){
+                for(int transaction = 1; transaction <= 2; transaction++){
+                    if(j){
+                        curr[j][transaction] = max(next[0][transaction] - prices[i], next[1][transaction]);
+                    }else{
+                        curr[j][transaction] = max(prices[i] + next[1][transaction-1], next[0][transaction]);
+                    }
+                }
+            }
+            next = curr;
+        }
+        return curr[1][2];
+    }
+*/
