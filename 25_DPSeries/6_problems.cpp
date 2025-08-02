218dbddd0cc03430414a079da62d9a51e2fd56ee
Ishushan02 <ishananand2002@gmail.com>
2025-08-02 13:28:10 -0700

    DP Series: Matrix Chain Multiplications

--- 25_DPSeries/6_problems.cpp
+++ 25_DPSeries/6_problems.cpp
@@ -3,3 +3,37 @@
     Partition DP's VVIII and it's Tough
 
 */
+
+/*
+    Matrix Chain Multiplication
+    (https://www.geeksforgeeks.org/problems/matrix-chain-multiplication0303/1)
+
+    int minMatMulCost(vector<int> &arr, int i, int j, vector<vector<int>> &dpArray){
+        if(i == j){
+            return 0;
+        }
+        
+        if(dpArray[i][j] != -1){
+            return dpArray[i][j];
+        }
+        
+        int minSteps = 1e9;
+        for(int k = i; k < j; k++){
+            int steps = arr[i-1] * arr[k] * arr[j] + minMatMulCost(arr, i, k, dpArray) + minMatMulCost(arr, k+1, j, dpArray);
+            minSteps = min(minSteps, steps);
+        }
+        
+        dpArray[i][j] = minSteps;
+        
+        return dpArray[i][j];
+        
+    }
+  
+    int matrixMultiplication(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        vector<vector<int>> dpArray(n+1, vector<int>(n+1, -1));
+        return minMatMulCost(arr, 1, n-1, dpArray);
+    }
+
+*/
