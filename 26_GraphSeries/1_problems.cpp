5bd0e22f4a48c2a40c67c02a6719c507c6d438bb
Ishushan02 <ishananand2002@gmail.com>
2025-08-11 13:40:32 -0700

    Graph Series: DFS Traversal

--- 26_GraphSeries/1_problems.cpp
+++ 26_GraphSeries/1_problems.cpp
@@ -1,5 +1,5 @@
 /*
-    841. Keys and Rooms
+    841. Keys and Rooms (BFS Traversal)
     (https://leetcode.com/problems/keys-and-rooms/description/)
 
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
@@ -27,4 +27,37 @@
 
         return rooms.size()==roomKeys.size();
     }
+*/
+
+/*
+    DFS Traversal
+    (https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1)
+
+    void dfsTraversal(vector<vector<int>>& adj, int node, unordered_map<int, bool> &visited, vector<int> &ans){
+        visited[node] = true;
+        ans.push_back(node);
+        for(int v: adj[node]){
+            if(visited[v] == false){
+                dfsTraversal(adj, v, visited, ans);
+            }
+        }
+        
+    }
+  
+    vector<int> dfs(vector<vector<int>>& adj) {
+        // Code here
+        
+        unordered_map<int, bool> visited;
+        vector<int> ans;
+        
+        for(int i = 0; i < adj.size(); i++){
+            if(visited[i] == false){
+                dfsTraversal(adj, i, visited, ans);
+            }
+        }
+        
+        return ans;
+        
+    }
+        
 */
