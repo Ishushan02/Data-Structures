d7c755e1df25472506d1768fcfc73cd36f6c9336
Ishushan02 <ishananand2002@gmail.com>
2025-08-18 06:58:17 -0700

    GraphSeries: Shortest Path in Directed Acyclic Graph

--- 26_GraphSeries/2_problems.cpp
+++ 26_GraphSeries/2_problems.cpp
@@ -83,3 +83,64 @@
     }
         
 */
+
+/*
+    Shortest path in Directed Acyclic Graph
+    (https://www.geeksforgeeks.org/problems/shortest-path-in-undirected-graph/1)
+
+    vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
+        // code here
+        vector<int> pathCost(V, INT_MAX);
+        unordered_map<int, vector<pair<int, int>>> graph;
+        vector<int> indegree(V, 0);
+        
+        for(int i = 0; i < E; i++){
+            graph[edges[i][0]].push_back({edges[i][1], edges[i][2]});
+            indegree[edges[i][1]] += 1;
+        }
+        
+        queue<pair<int, int>> que;
+        for(int i = 0; i < V; i++){
+            if(indegree[i] == 0){
+                que.push({i, 0});
+            }
+        }
+        vector<int> topOrder;
+        pathCost[0] = 0;
+        while(!que.empty()){
+            auto fr = que.front();
+            int node = fr.first;
+            int wt = fr.second;
+            que.pop();
+            topOrder.push_back(node);
+            for(auto &v:graph[node]){
+                indegree[v.first]--;
+                if(indegree[v.first] == 0){
+                    que.push(v);
+                }
+            }
+        }
+        
+        for(auto v:topOrder){
+            int wt = pathCost[v];
+            if(wt != INT_MAX){
+                for(auto eachNode:graph[v]){
+                    int node = eachNode.first;
+                    int cost = eachNode.second;
+                    if (wt + cost < pathCost[node]) {
+                        pathCost[node] = cost + wt;
+                    }
+                }
+            }
+            
+        }
+        
+        for(int i = 0; i < V; i++){
+            if(pathCost[i] == INT_MAX){
+                pathCost[i] = -1;
+            }
+        }
+        
+        return pathCost;
+    }
+*/
