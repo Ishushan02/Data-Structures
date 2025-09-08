b70fb2a72bcc84f2955b8ca15b1ad9c5bab29722
Ishushan02 <ishananand2002@gmail.com>
2025-09-08 10:21:23 -0700

    Revise: Longest Parentheses

--- ZRevision/1_problems.cpp
+++ ZRevision/1_problems.cpp
@@ -123,3 +123,29 @@
         return ans;
     }
 */
+
+/*
+
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        int maxCount = 0;
+        st.push(-1);
+        for(int i = 0; i < s.length(); i++){
+
+            if(s[i] == '('){
+                st.push(i);
+            }else{
+                st.pop();
+
+                if(st.empty()){
+                    st.push(i);
+                }else{
+                    maxCount = max(maxCount, i - st.top());
+                }
+                
+            }
+        }
+
+        return maxCount;
+    }
+*/
