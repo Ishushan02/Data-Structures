f759f24d67d837598053e3fdf5b1c7c7016c70d7
Ishushan02 <ishananand2002@gmail.com>
2025-09-09 12:24:21 -0700

    Revise: 132 Patterns

--- ZRevision/1_problems.cpp
+++ ZRevision/1_problems.cpp
@@ -149,3 +149,28 @@
         return maxCount;
     }
 */
+
+
+/*
+    456. 132 Pattern
+    (https://leetcode.com/problems/132-pattern/)
+
+    bool find132pattern(vector<int>& nums) {
+        
+        stack<int> st;
+        int third = -1e9;
+        for(int i =  nums.size() -1 ; i >= 0; i--){
+            
+            if(third > nums[i]) return true;
+
+            while(!st.empty() && st.top() < nums[i]){
+                third = st.top();
+                st.pop();
+            }
+            st.push(nums[i]);
+        }
+
+        return false;
+    }
+        
+*/
