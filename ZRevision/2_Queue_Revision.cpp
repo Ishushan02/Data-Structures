46eb3003c2dfd4c68c69d0b62c3ebcb9b0e4ec09
Ishushan02 <ishananand2002@gmail.com>
2025-09-14 17:43:40 -0700

    Revision: Stream First Non Repeating

--- ZRevision/2_Queue_Revision.cpp
+++ ZRevision/2_Queue_Revision.cpp
@@ -39,3 +39,40 @@
     }
 
 */
+
+
+/*
+
+    Stream First Non-repeating
+    (https://www.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1 )
+
+    string FirstNonRepeating(string &s) {
+        // Code here
+        
+        unordered_map<char, int> charCount;
+        queue<char> que;
+        string ans;
+        
+        for(int i = 0; i < s.length(); i++){
+            charCount[s[i]] += 1;
+            que.push(s[i]);
+            
+            while(!que.empty()){
+                
+                if(charCount[que.front()] == 1){
+                    ans += que.front();
+                    break;
+                }else{
+                    que.pop();
+                }
+            }
+            
+            if(que.empty()){
+                ans += '#';
+            }
+            
+        }
+        
+        return ans;
+    }
+*/
