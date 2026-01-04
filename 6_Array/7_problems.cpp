3df9861dd8d87eba94136f094d61152c4c248731
Ishushan02 <ishananand2002@gmail.com>
2026-01-04 14:24:12 -0800

    Matrix Representation

--- 6_Array/7_problems.cpp
+++ 6_Array/7_problems.cpp
@@ -55,3 +55,25 @@ int main()
 }
 
 */
+
+/*
+
+YOU FORGET THIS YOU IDIOT, A MATRIX OF R ROWS AND C COLS, EACH POSITION OF MATRIX CAN BE WRITTEN AS
+
+    
+
+    [
+        [[0, 0], [0, 1], [0, 2]],
+        [[1, 0], [1, 1], [1, 2]],
+        [[2, 0], [2, 1], [2, 2]]
+    ]
+
+    ===> C * I + J 
+
+    [
+        [[0, 1, 2]],
+        [[3, 4, 5]],
+        [[6, 7, 8]]
+    ]
+
+*/
