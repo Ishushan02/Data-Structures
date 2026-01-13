f249d9db961462a90c0dbb73c27850bf5f3b6a80
Ishushan02 <ishananand2002@gmail.com>
2026-01-13 11:03:54 -0800

    Good Method to Check Prime

--- 12_Maths/1_maths.cpp
+++ 12_Maths/1_maths.cpp
@@ -2,6 +2,27 @@
 
 /*
 
+    1 Good Method to check whether a Number is Prime
+
+    bool isPrime(int n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+    
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+        return true;
+
+    }
+
+*/
+
+
+
+/*
+
 IMP Question
 
 204. Count Primes (https://leetcode.com/problems/count-primes/)
