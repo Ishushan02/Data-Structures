a9dc4a75bb0f699c8fab9a9a685b44d5a4f1cacf
Ishushan02 <ishananand2002@gmail.com>
2026-02-06 07:05:36 -0800

    Binary Search Approach for LIS

--- 25_DPSeries/3_problems.cpp
+++ 25_DPSeries/3_problems.cpp
@@ -105,6 +105,29 @@
 
         return dpVal[0];
     }
+
+
+    Binary Search Approach for LIS
+    int binarySearchApproach(vector<int>& nums){
+        vector<int> ans;
+        ans.push_back(nums[0]);
+
+        for(int i = 1; i<nums.size(); i++){
+            if(nums[i] > ans.back()){
+                ans.push_back(nums[i]);
+            }else{
+                int idx = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+                ans[idx] = nums[i];
+            }
+
+            for(auto v: ans){
+                cout << v << " ";
+            }cout<<endl;
+        }
+
+        return ans.size();
+    }
+        
 */
 
 /*
