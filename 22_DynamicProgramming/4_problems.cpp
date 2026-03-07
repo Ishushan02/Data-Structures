a2f05fa415eca60607948029d33b151bb0850732
Ishushan02 <ishananand2002@gmail.com>
2026-03-06 18:44:53 -0800

    Solved Russian Doll

--- 22_DynamicProgramming/4_problems.cpp
+++ 22_DynamicProgramming/4_problems.cpp
@@ -259,4 +259,33 @@
 
     }
 
+    // Solved Solution
+    struct comp{
+        bool operator()(vector<int> &a, vector<int> &b){
+            if(a[0] == b[0]){
+                return a[1] > b[1];
+            }
+
+            return a[0] < b[0];
+        }
+    };
+
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), comp());
+        int n = envelopes.size();
+
+        vector<int> temp;
+        temp.push_back(envelopes[0][1]);
+        for(int i = 1; i < n; i++){
+            auto idx = lower_bound(temp.begin(), temp.end(), envelopes[i][1]);
+            if (idx == temp.end()) {
+                temp.push_back(envelopes[i][1]);
+            } else {
+                *idx = envelopes[i][1];
+            }
+        }
+
+        return temp.size();
+    }
+
 */
