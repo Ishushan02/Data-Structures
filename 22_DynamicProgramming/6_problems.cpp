362d745b813263872b469d7691d8892639325d6d
Ishushan02 <ishananand2002@gmail.com>
2026-03-07 09:24:23 -0800

    Wild Card Matching - Efficient wway

--- 22_DynamicProgramming/6_problems.cpp
+++ 22_DynamicProgramming/6_problems.cpp
@@ -90,6 +90,47 @@
         vector<vector<int>> dpArr(m+1, vector<int>(n+1, -1));
         return stringMatchDP(s, p, 0, 0, dpArr);
     }
+
+
+    // Most Efficient Way
+    bool isMatch(string s, string p) {
+        // return matchString(s, p, 0, 0);
+
+        int m = s.length();
+        int n = p.length();
+
+        vector<int> currRow(n + 1, 0);
+        vector<int> forwRow(n + 1, 0);
+        forwRow[n] = true;
+
+        for(int j = n - 1; j >= 0; j--){
+            if(p[j] == '*'){
+                forwRow[j] = forwRow[j + 1];
+            }
+        }
+        
+
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                bool ans = false;
+
+                if(s[i] == p[j] || p[j] == '?'){
+                    ans = forwRow[j+1];
+                }else if(p[j] == '*'){
+                    
+                    bool ans1 = currRow[j+1];
+                    bool ans2 = forwRow[j];
+
+                    ans = ans1 || ans2;
+                }
+                currRow[j] = ans;
+            }
+            forwRow = currRow;
+        }
+
+        return forwRow[0];
+
+    }
 */
 
 
