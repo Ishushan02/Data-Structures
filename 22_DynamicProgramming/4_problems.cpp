a2282ba71e47d1b68de6b454ae12ada25ce961d3
Ishushan02 <ishananand2002@gmail.com>
2026-03-06 17:01:01 -0800

    LCS - Tabulation most efficient Approach

--- 22_DynamicProgramming/4_problems.cpp
+++ 22_DynamicProgramming/4_problems.cpp
@@ -51,6 +51,36 @@
         return getMaxSubsequenceDP(text1, text2, 0, 0, dpArr);
     }
 
+
+    // Tabulation Approach
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        // vector<vector<int>> dpArray(m + 1, vector<int>(n + 1, 0));
+        // return lcs(text1, text2, 0, 0);
+        vector<int> currVal(n + 1, 0);
+        vector<int> forwVal(n + 1, 0);
+
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                int maxLen = 0;
+                if(text1[i] == text2[j]){
+                    maxLen = 1 + forwVal[j+1];
+                }else{
+                    auto x = forwVal[j];
+                    auto y = currVal[j+1];
+                    maxLen = max({maxLen, x, y});
+                }
+
+                currVal[j] = maxLen;
+            }
+            forwVal = currVal;
+        }
+
+        return forwVal[0];
+    }
+
 */
 
 
